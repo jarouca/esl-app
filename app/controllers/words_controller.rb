fd0cfbc3ea533471498edce4048200516475c319
Jonathan Arouca <jonathanarouca@gmail.com>
2017-06-26 22:10:24 -0400

    add user adds word to bank (happy path)

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,19 +1,33 @@
-
 class WordsController < ApplicationController
 
   def create
+    @bank = Bank.find(params["bank_id"])
 
+    new_word = params["word"]["word"]
 
     response = HTTParty.get(
-  "https://wordsapiv1.p.mashape.com/words/incredible/definitions",
-  headers:{
+      "https://wordsapiv1.p.mashape.com/words/#{new_word}/definitions",
+      headers:{
       "X-Mashape-Key" => "i0cjA2j435mshSHKQEDvu6jNERWzp1wNjlRjsnxXRFfpg0il5D",
       "Accept" => "application/json"
-    }
-  )
+      }
+    ).parsed_response
+
+    if @bank.words << Word.new(
+      word: new_word,
+      definition: response["definitions"][0]["definition"],
+      part_of_speech: response["definitions"][0]["partOfSpeech"]
+      )
+
+      flash[:notice] = "Word added successfully."
+      redirect_to @bank
+    end
 
 
-
-      binding.pry
   end
 end
+
+# params["bank_id"]
+# response["word"]
+# response["definitions"][0]["definition"]
+# response["definitions"][0]["partOfSpeech"]
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,7 @@
 class Word < ApplicationRecord
   has_and_belongs_to_many :banks
 
+  validates :word, presence: true
   validates :part_of_speech, presence: true
   validates :definition, presence: true
 end
