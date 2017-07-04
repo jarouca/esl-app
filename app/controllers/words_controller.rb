3db7bb14cd2550d26b8ed74a232ec0b5eb7737ab
Jonathan Arouca <jonathanarouca@gmail.com>
2017-07-04 15:24:55 -0400

    style add word feature and bank#show

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -31,17 +31,17 @@ class WordsController < ApplicationController
   def create
     @bank = Bank.find(params["bank_id"])
 
-    definition = params["word"].split(",")
+      definition = params["word"].split(",")
 
-    if @bank.words << Word.new(
-      word: definition[0],
-      definition: definition[2],
-      part_of_speech: definition[1]
-      )
+      if @bank.words << Word.new(
+        word: definition[0],
+        definition: definition[2],
+        part_of_speech: definition[1]
+        )
 
-      flash[:notice] = "Word added successfully."
-      redirect_to @bank
-    end
+        flash[:notice] = "Word added successfully."
+        redirect_to @bank
+      end
   end
 
   def destroy
